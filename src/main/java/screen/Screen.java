c622b7792ba2d0fe38beaab17c06c84d15e8a615
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-15 21:14:40 +0900

    feat: 상영 계획표 필드 추가

--- src/main/java/screen/Screen.java
+++ src/main/java/screen/Screen.java
@@ -3,6 +3,7 @@ package screen;
 import java.time.LocalDateTime;
 import java.util.List;
 import movie.enums.ScreeningType;
+import screen.screening.ScreeningSchedule;
 import seat.Seat;
 
 public class Screen {
@@ -10,19 +11,18 @@ public class Screen {
     private Long code;
     private Enum<ScreeningType> screeningType;
     private List<Seat> seats;
-    private Long movieCode;
-    private List<LocalDateTime> movieTimeTable;
     private LocalDateTime operatingTime;
     private int screenPrice;
+    private ScreeningSchedule screeningSchedule;
+
 
     public Screen(ScreenBuilder builder) {
         this.code = builder.code;
         this.screeningType = builder.screeningType;
         this.seats = builder.seats;
-        this.movieCode = builder.movieCode;
-        this.movieTimeTable = builder.movieTimeTable;
         this.operatingTime = builder.operatingTime;
         this.screenPrice = builder.screenPrice;
+        this.screeningSchedule = builder.screeningSchedule;
     }
 
 
@@ -31,9 +31,8 @@ public class Screen {
         private Long code;
         private Enum<ScreeningType> screeningType;
         private List<Seat> seats;
-        private Long movieCode;
-        private List<LocalDateTime> movieTimeTable;
         private LocalDateTime operatingTime;
+        private ScreeningSchedule screeningSchedule;
         private int screenPrice;
 
         public ScreenBuilder() {
@@ -58,16 +57,6 @@ public class Screen {
             return this;
         }
 
-        public ScreenBuilder withMovieCode(Long movieCode) {
-            this.movieCode = movieCode;
-            return this;
-        }
-
-        public ScreenBuilder withMovieTimeTable(List<LocalDateTime> movieTimeTable) {
-            this.movieTimeTable = movieTimeTable;
-            return this;
-        }
-
         public ScreenBuilder withOperatingTime(LocalDateTime operatingTime) {
             this.operatingTime = operatingTime;
             return this;
@@ -78,8 +67,62 @@ public class Screen {
             return this;
         }
 
+        public ScreenBuilder withScreeningSchedule(ScreeningSchedule screeningSchedule) {
+            this.screeningSchedule = screeningSchedule;
+            return this;
+        }
+
         public Screen build() {
             return new Screen(this);
         }
+    }
+
+    public Screen setScreeningType(Enum<ScreeningType> screeningType) {
+        this.screeningType = screeningType;
+        return this;
+    }
+
+    public Screen setSeats(List<Seat> seats) {
+        this.seats = seats;
+        return this;
+    }
+
+    public Screen setOperatingTime(LocalDateTime operatingTime) {
+        this.operatingTime = operatingTime;
+        return this;
+    }
+
+    public Screen setScreenPrice(int screenPrice) {
+        this.screenPrice = screenPrice;
+        return this;
+    }
+
+    public Screen setScreeningSchedule(ScreeningSchedule screeningSchedule) {
+        this.screeningSchedule = screeningSchedule;
+        return this;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public Enum<ScreeningType> getScreeningType() {
+        return screeningType;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public LocalDateTime getOperatingTime() {
+        return operatingTime;
+    }
+
+    public int getScreenPrice() {
+        return screenPrice;
+    }
+
+    public ScreeningSchedule getScreeningSchedule() {
+        return screeningSchedule;
     }
 }
