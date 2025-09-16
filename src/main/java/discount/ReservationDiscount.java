94beb2abe63ebc53ba6a4666ec5102b43bc01843
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-16 19:31:29 +0900

    feat: 할인 로직 추가

--- src/main/java/discount/ReservationDiscount.java
+++ src/main/java/discount/ReservationDiscount.java
@@ -1,29 +1,44 @@
 package discount;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class ReservationDiscount {
 
-    private final Long userId;
-    private final LocalDateTime screenTime;
+    private Long Id;
+    private LocalDateTime screeningStartTime;
     private int totalPrice;
+    private int userPoint;
+    private static final int TIME_DISCOUNT_PRICE = 2000;
 
-    public ReservationDiscount(Long userId, LocalDateTime screenTime) {
-        this.userId = userId;
-        this.screenTime = screenTime;
+
+    public int discountByTime(LocalDateTime screeningStartTime, int totalPrice) {
+        LocalDateTime beforeNoonDiscountTime = LocalDateTime.of(screeningStartTime.getYear(),
+            screeningStartTime.getMonth(), screeningStartTime.getDayOfMonth(), 11, 0, 0);
+        if (screeningStartTime.isBefore(beforeNoonDiscountTime)
+            || 20 <= screeningStartTime.getHour()) {
+            totalPrice -= TIME_DISCOUNT_PRICE;
+            return totalPrice;
+        }
+        return totalPrice;
     }
 
-    public ReservationDiscount(Long userId, LocalDateTime screenTime, int totalPrice) {
-        this.userId = userId;
-        this.screenTime = screenTime;
-        this.totalPrice = totalPrice;
+    public int discountByPoint(int totalPrice, int userPoint) {
+        if (userPoint >= totalPrice) {
+            return 0;
+        }
+        totalPrice -= userPoint;
+        return totalPrice;
     }
 
-    // TODO: 조조할인 적용하여 정액 할인
-
-    // TODO: 포인트 사용하여 정액 할인
-
-    // TODO: 무비데이 10% 할인
+    public int discountByMovieDay(int totalPrice) {
+        if (LocalDate.now().getDayOfMonth() % 10 == 0) {
+            double price = totalPrice;
+            price *= 0.9;
+            totalPrice = (int) price;
+        }
+        return totalPrice;
+    }
 
 
 }
