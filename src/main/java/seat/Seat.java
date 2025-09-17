e74e920ea136ecbdd5fc191ffbf699b2d98a7fd9
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-17 16:33:13 +0900

    feat: 좌석 도메인 재설계

--- src/main/java/seat/Seat.java
+++ src/main/java/seat/Seat.java
@@ -3,15 +3,32 @@ package seat;
 import seat.enums.SeatGrade;
 
 public class Seat {
-    private char row;
+
+    private String row;
     private int col;
     private boolean isOccupied;
-    private Enum<SeatGrade> seatGrade;
+    private SeatGrade seatGrade;
 
-    public Seat(char row, int col, boolean isOccupied, Enum<SeatGrade> seatGrade) {
+    public Seat(String row, int col, boolean isOccupied, SeatGrade seatGrade) {
         this.row = row;
         this.col = col;
         this.isOccupied = isOccupied;
         this.seatGrade = seatGrade;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public SeatGrade getSeatGrade() {
+        return seatGrade;
     }
 }
