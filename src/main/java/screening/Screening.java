472984420d3447fe086cf5e40e7254f07253f2c6
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-17 16:33:27 +0900

    feat: 상영 도메인 재설계

--- src/main/java/screening/Screening.java
+++ src/main/java/screening/Screening.java
@@ -1,77 +1,33 @@
 package screening;
 
 import java.time.LocalDateTime;
-import java.util.Map;
+import java.util.List;
 import movie.Movie;
 import seat.Seat;
 
 public class Screening {
 
-    private Long id;
     private Movie movie;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private Map<Seat, Boolean> seatReserved;
-    private int price;
+    private LocalDateTime ScreeningTime; // 영화 시간 + 상영 시작 시간
+    private List<Seat> seats;
 
-    public Screening() {
-    }
-
-    public Screening(Movie movie, LocalDateTime startTime, LocalDateTime endTime,
-        Map<Seat, Boolean> seatReserved, int price) {
+    public Screening(Movie movie, LocalDateTime screeningTime, List<Seat> seats) {
         this.movie = movie;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.seatReserved = seatReserved;
-        this.price = price;
+        ScreeningTime = screeningTime;
+        this.seats = seats;
     }
 
     public Movie getMovie() {
         return movie;
     }
 
-    public Screening setMovie(Movie movie) {
-        this.movie = movie;
-        return this;
+    public LocalDateTime getScreeningTime() {
+        return ScreeningTime;
     }
 
-    public LocalDateTime getStartTime() {
-        return startTime;
+    public List<Seat> getSeats() {
+        return seats;
     }
 
-    public Screening setStartTime(LocalDateTime startTime) {
-        this.startTime = startTime;
-        return this;
-    }
 
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public Screening setEndTime(LocalDateTime endTime) {
-        this.endTime = endTime;
-        return this;
-    }
-
-    public Map<Seat, Boolean> getSeatReserved() {
-        return seatReserved;
-    }
-
-    public Screening setSeatReserved(Map<Seat, Boolean> seatReserved) {
-        this.seatReserved = seatReserved;
-        return this;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public Screening setPrice(int price) {
-        this.price = price;
-        return this;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
