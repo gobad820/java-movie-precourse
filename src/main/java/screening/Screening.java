b27f11aa443b00c34e15fdc0956480ef0d50de7f
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-19 01:14:48 +0900

    feat: 상영 좌석 할당 로직 추가

--- src/main/java/screening/Screening.java
+++ src/main/java/screening/Screening.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import movie.Movie;
 import seat.Seat;
+import seat.enums.SeatGrade;
 
 public class Screening {
 
@@ -29,13 +30,44 @@ public class Screening {
         return seats;
     }
 
-    public Boolean checkSeatOccupied(String row, int col) {
+    public Boolean checkSeatOccupied(String seatNumber) {
         for (Seat s : seats) {
-            if (s.getRow().equals(row) && s.getCol() == col) {
+            if (s.getSeatNumber().equals(seatNumber)) {
                 return s.isOccupied();
             }
         }
-        return true;
+        throw new RuntimeException("없는 좌석입니다.");
+    }
+
+    public void allocateSeats(int row, int col) {
+        int totalSeat = row * col;
+        int s = (int) (totalSeat * 0.2);
+        int a = (int) (totalSeat * 0.3);
+        int b = totalSeat - s - a;
+        int [] ratio = {s, a, b};
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 1; j <= col; j++) {
+                String seatNumber = makeSeatNumberString(i, j);
+                seats.add(createSeat(ratio, seatNumber));
+            }
+        }
+    }
+
+    private static Seat createSeat(int [] ratio, String seatNumber) {
+        if (ratio[0]-- > 0) {
+            return new Seat(seatNumber, false, SeatGrade.S);
+        }
+        if (ratio[1]-- > 0) {
+            return new Seat(seatNumber, false, SeatGrade.A);
+        }
+        return new Seat(seatNumber, false, SeatGrade.B);
+    }
+
+    private static String makeSeatNumberString(int i, int j) {
+        String seatRow = "A" + i;
+        String seatNumber = seatRow + Integer.toString(j);
+        return seatNumber;
     }
 
 
