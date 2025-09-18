afca2d6f825a5dc3a4043e7700d22e6343afb622
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-19 02:09:04 +0900

    feat: 중복 할인 테스트 추가

--- src/test/java/reservation/ReservationTest.java
+++ src/test/java/reservation/ReservationTest.java
@@ -31,9 +31,10 @@ class ReservationTest {
     PaymentType paymentType = PaymentType.CREDIT_CARD;
     Movie movie = new Movie("기생충", Duration.ofHours(1).plusMinutes(30));
     LocalDateTime localDateTime = LocalDateTime.of(2025, 9, 19, 15, 0);
-    Screening screening = new Screening(movie, localDateTime, new ArrayList<>(
+    List<Seat> seats = new ArrayList<>(
         List.of(new Seat("A1", false, SeatGrade.S), new Seat("A2", false, SeatGrade.S),
-            new Seat("A3", false, SeatGrade.S))));
+            new Seat("A3", false, SeatGrade.S)));
+    Screening screening = new Screening(movie, localDateTime, seats);
 
     @Test
     @DisplayName("예약 생성 테스트")
@@ -123,6 +124,82 @@ class ReservationTest {
             int price = (int) (SeatGrade.S.getPrice() * 0.98);
             assertAll(() -> assertThat(reservation.getTotalPrice()).isEqualTo(price));
         }
+    }
+
+    @Nested
+    @DisplayName("조조 및 야간 영화 활인 테스트")
+    class morningAndNightTest {
+
+        private static final int TIME_DISCOUNT = 2000;
+
+        @ParameterizedTest
+        @ValueSource(ints = {8, 9, 10})
+        @DisplayName("조조 영화 할인 테스트")
+        public void morningDiscountTest(int hour) {
+            // given
+            Screening discountScreening = new Screening(movie,
+                LocalDateTime.of(2025, 9, 15, hour, 30), seats);
+            UserReservationRequest reservationRequest = new UserReservationRequest(
+                discountScreening, List.of("A1", "A2"), SeatGrade.S);
+            Reservation reservation = new Reservation(user, List.of(reservationRequest),
+                isPointedUsed, paymentType, discountScreening);
+            // when
+            int beforeDiscountPrice =
+                reservationRequest.seatNumber().size() * reservationRequest.seatGrade().getPrice();
+            int priceTimeDiscount = (int) reservation.getPriceTimeDiscount();
+            // then
+            assertThat(priceTimeDiscount).isEqualTo(
+                beforeDiscountPrice - (TIME_DISCOUNT * reservationRequest.seatNumber().size()));
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {20, 21, 22, 23})
+        @DisplayName("심야 영화 할인 테스트")
+        public void nightDiscountTest(int hour) {
+            // given
+            Screening discountScreening = new Screening(movie,
+                LocalDateTime.of(2025, 9, 15, hour, 30), seats);
+            UserReservationRequest reservationRequest = new UserReservationRequest(
+                discountScreening, List.of("A1", "A2"), SeatGrade.S);
+            Reservation reservation = new Reservation(user, List.of(reservationRequest),
+                isPointedUsed, paymentType, discountScreening);
+            // when
+            int beforeDiscountPrice =
+                reservationRequest.seatNumber().size() * reservationRequest.seatGrade().getPrice();
+            int priceTimeDiscount = (int) reservation.getPriceTimeDiscount();
+            // then
+            assertThat(priceTimeDiscount).isEqualTo(
+                beforeDiscountPrice - (TIME_DISCOUNT * reservationRequest.seatNumber().size()));
+        }
+    }
+
+    @Nested
+    @DisplayName("중복 할인 테스트")
+    class DuplicatedDiscountTestClass {
+
+        private static final int TIME_DISCOUNT = 2000;
+
+        @ParameterizedTest
+        @ValueSource(ints = {8, 10})
+        @DisplayName("신용카드 조조할인 무비데이 중복 테스트")
+        public void morningAndCreditCardTest(int morningHour) {
+            // given
+            Screening discountScreening = new Screening(movie,
+                LocalDateTime.of(2025, 9, 20, morningHour, 30), seats);
+            UserReservationRequest reservationRequest = new UserReservationRequest(
+                discountScreening, List.of("A1", "A2"), SeatGrade.S);
+            Reservation reservation = new Reservation(user, List.of(reservationRequest),
+                isPointedUsed, paymentType,
+                discountScreening);
+            // when
+            reservation.discountTotalPrice();
+            int expectedPrice = (int) ((SeatGrade.S.getPrice() - TIME_DISCOUNT)
+                * reservationRequest.seatNumber().size() * 0.9 * 0.95);
+
+            // then
+            assertThat(reservation.getTotalPrice()).isEqualTo(expectedPrice);
+        }
+
     }
 
 }
