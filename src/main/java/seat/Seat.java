2c1be60a6b5cffccb52f0fc03b91d2ba509f6197
Kim Sanghae <pelikan@pusan.ac.kr>
2025-09-19 01:14:14 +0900

    refactor: 좌석 번호 String으로 변경
    - 행열을 합친 상태로 바로 입력 될 수 있도록 변경

--- src/main/java/seat/Seat.java
+++ src/main/java/seat/Seat.java
@@ -4,24 +4,18 @@ import seat.enums.SeatGrade;
 
 public class Seat {
 
-    private String row;
-    private int col;
+    private String seatNumber;
     private boolean isOccupied;
     private SeatGrade seatGrade;
 
-    public Seat(String row, int col, boolean isOccupied, SeatGrade seatGrade) {
-        this.row = row;
-        this.col = col;
+    public Seat(String seatNumber, boolean isOccupied, SeatGrade seatGrade) {
+        this.seatNumber = seatNumber;
         this.isOccupied = isOccupied;
         this.seatGrade = seatGrade;
     }
 
-    public String getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
+    public String getSeatNumber() {
+        return seatNumber;
     }
 
     public boolean isOccupied() {
